ea7d063e9c4656c9591dba199ab3db12dd607bf1
daniel-dron <danieldron2@gmail.com>
2024-07-02 23:03:29 +0100

    commands

--- build/src/CMakeFiles/engine.dir/vk_images.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/engine.dir/vk_images.cpp.o: \
- /home/danield/projects/vkguide/src/vk_images.cpp \
- /usr/include/stdc-predef.h \
- /home/danield/projects/vkguide/src/vk_images.h
--- build/src/CMakeFiles/engine.dir/vk_loader.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/engine.dir/vk_loader.cpp.o: \
- /home/danield/projects/vkguide/src/vk_loader.cpp \
- /usr/include/stdc-predef.h \
- /home/danield/projects/vkguide/src/vk_loader.h
