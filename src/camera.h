0e93be1d0249c4311fef9177c8199723d816448b
Daniel Dron <danieldron2@gmail.com>
2024-07-17 12:24:45 +0100

    remove unused camera class

--- src/camera.h
+++ src/camera.h
@@ -2,24 +2,6 @@
 #include "SDL_events.h"
 #include <vk_types.h>
 
-class Camera {
-public:
-  glm::vec3 velocity;
-  glm::vec3 position;
-
-  // vertical rotation
-  float pitch{0.0f};
-  // horizontal rotation
-  float yaw{0.0f};
-
-  glm::mat4 getViewMatrix();
-  glm::mat4 getRotationMatrix();
-
-  void processSDLEvent(SDL_Event &e);
-
-  void update();
-};
-
 class Camera3D
 {
 public:
