cc3e52c9acbf13efffeff81ffacbc150fffe22de
Daniel Dron <danieldron2@gmail.com>
2024-07-22 15:36:54 +0100

    alias glm

--- src/math/transform.h
+++ src/math/transform.h
@@ -2,29 +2,33 @@
 
 #include <vk_types.h>
 
+using vec3 = glm::vec3;
+using mat4 = glm::mat4;
+using quat = glm::quat;
+
 class Transform3D {
  public:
-  const glm::vec3& get_position() const;
-  const glm::quat& get_heading() const;
-  const glm::vec3& get_scale() const;
+  const vec3& get_position() const;
+  const quat& get_heading() const;
+  const vec3& get_scale() const;
 
-  void set_position(const glm::vec3& pos);
-  void set_heading(const glm::quat& h);
-  void set_scale(const glm::vec3& s);
+  void set_position(const vec3& pos);
+  void set_heading(const quat& h);
+  void set_scale(const vec3& s);
 
-  const glm::vec3& get_position();
-  const glm::quat& get_heading();
-  const glm::vec3& get_scale();
+  const vec3& get_position();
+  const quat& get_heading();
+  const vec3& get_scale();
 
-  glm::vec3 get_local_up();
-  glm::vec3 get_local_right();
-  glm::vec3 get_local_front();
+  vec3 get_local_up();
+  vec3 get_local_right();
+  vec3 get_local_front();
 
  private:
-  glm::vec3 position{};
-  glm::quat heading = glm::identity<glm::quat>();
-  glm::vec3 scale{1.0f};
+  vec3 position{};
+  quat heading = glm::identity<quat>();
+  vec3 scale{1.0f};
 
-  mutable glm::mat4 matrix = glm::identity<glm::mat4>();
+  mutable mat4 matrix = glm::identity<mat4>();
   mutable bool is_dirty = false;
 };
